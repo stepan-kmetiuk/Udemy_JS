d6073476027effbaafd753b138107b18c541fd8b
Stepan <kmetuk60187@gmail.com>
2024-11-13 01:19:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// snake_case
+// UPPER_SNAKE_CASE
+// kebab-case
+// PascaleCase
+
+// lessons 12
+
+// const result = confirm('are you here');
+// console.log(result);
+
+// const answer = +prompt('тобі є 18?', '2');
+// console.log(answer + 4);
+
+// const answers = [];
+
+// answers[0] = prompt('kak wahe imiya', ' ');
+// answers[1] = prompt('kak wahe familia', ' ');
+// answers[2] = prompt('srilru toby rokiv', ' ');
+
+// document.write(answers);
+
+// let incr = 10;
+
+// console.log(incr--);
